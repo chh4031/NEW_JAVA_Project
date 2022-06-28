7577d58053411895880f2a0a92eacadb63acda75
chh4031 <chh4031@gmail.com>
2022-06-28 10:52:57 +0900

    4장

---
+++ chap04/src/section04_01/quiz02_p147.java
@@ -0,0 +1,16 @@
+package section04_01;
+
+public class quiz02_p147 {
+	public static void main(String[] args) {
+		System.out.println("¾î¶² ÇýÅÃ?");
+		
+		char grade = 'C';
+		switch(grade) {
+		case 'A':System.out.println("VVIP");break;
+		case 'B':System.out.println("VIP");break;
+		case 'C':System.out.println("Common");break;
+		case 'D':System.out.println("Normal");break;
+		default:System.out.println("undefined!");break;
+		}
+	}
+}
---
+++ chap04/src/section04_02/exam00_p149.java
@@ -0,0 +1,11 @@
+package section04_02;
+
+public class exam00_p149 {
+	public static void main(String[] args) {
+		int sum = 0;
+		for(int i = 0; i <= 100; i++) {
+			sum += i;
+		}
+		System.out.println("1~100ÀÇ ÇÕ : " + sum);
+	}
+}
