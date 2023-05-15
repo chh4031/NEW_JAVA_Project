e9482b47ae33e3ca4eed6c3262ca35fcee36545a
chh4031 <chh4031@gmail.com>
2023-05-15 23:31:43 +0900

    Singleton_07

---
+++ Softwaring/src/Design_sing07/User.java
@@ -0,0 +1,14 @@
+package Design_sing07;
+
+public class User{
+	public String Name;
+	public String Beverage;
+	public User(String Name, String Beverage) {
+		this.Name = Name;
+		this.Beverage = Beverage;
+	}
+	public void make() {
+		EspressoMachine EM = EspressoMachine.getInMachine();
+		EM.Brew(Name, Beverage);
+	}
+}
